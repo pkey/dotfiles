2da2c8f68d39b38ac48492808ffe8fc71d35ffaa
Paulius Kutka <paulius.kutka@swedbank.lt>
2018-06-29 17:39:14 +0300

    Partially implemented swed

---
+++ swedbank/swedbank.sh
@@ -0,0 +1,11 @@
+echo "Setting up swedbank environment..."
+
+#Setup swedbank aliases
+alias gcfeat='git commit -m "feat($(git_current_branch | cut -d / -f2)):'
+alias gcfix='git commit -m "fix($(git_current_branch | cut -d / -f2)):'
+
+#Setup config aliases
+alias swed='~/.scripts/swedbank/swedconfig'
+alias my='~/.scripts/swedbank/myconfig'
+chmod +x ~/.scripts/swedbank/swedconfig
+chmod +x ~/.scripts/swedbank/myconfig
