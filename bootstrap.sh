0eaebd3c71da524fc927626615ddddd61030c211
Paulius Kutka <kutka100@gmail.com>
2019-02-16 12:15:29 +0200

    Changes nvm version and adds some additional scripts to be executed in order to enable it[C

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,7 +48,10 @@ brew cleanup
 
 echo "Installing additional packages..."
 echo "Installing nvm"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo "Done installing packages"
 
