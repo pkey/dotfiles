c63b2c416dc585d29bd3ca1d5175848dbf73b989
Paulius Kutka <kutka100@gmail.com>
2019-09-08 16:23:18 +0300

    Changed bracked types to prevent runnin java_home command

--- aliases/main-alias.sh
+++ aliases/main-alias.sh
@@ -62,9 +62,10 @@ alias k='kubectl'
 alias toggle-fb='sudo ~/.dotfiles/productivity/toggle-fb'
 
 ### Java ###
+
 #alias j12="export JAVA_HOME=`/usr/libexec/java_home -v 12`; java -version"
-alias j11="export JAVA_HOME=`/usr/libexec/java_home -v 11`; java -version"
+alias j11="export JAVA_HOME='/usr/libexec/java_home -v 11'; java -version"
 #alias j10="export JAVA_HOME=`/usr/libexec/java_home -v 10`; java -version"
 #alias j9="export JAVA_HOME=`/usr/libexec/java_home -v 9`; java -version"
-alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
+alias j8="export JAVA_HOME='/usr/libexec/java_home -v 1.8'; java -version"
 #alias j7="export JAVA_HOME=`/usr/libexec/java_home -v 1.7`; java -version"
