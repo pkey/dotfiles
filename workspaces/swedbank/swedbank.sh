5bfe6fafd3a3feacb22a19a1064074293ff9a3ed
Paulius Kutka <paulius.kutka@swedbank.lt>
2019-09-09 12:42:18 +0300

    [skip travis] Updated README and removed old files

--- workspaces/swedbank/swedbank.sh
+++ workspaces/swedbank/swedbank.sh
@@ -1,16 +1,8 @@
 echo "Setting up swedbank environment..."
 
-currentDir=$(dirname $0)
-
 #Setup swedbank aliases
 alias gcfeat='git commit -m "feat($(git_current_branch | cut -d / -f2)):'
 alias gcfix='git commit -m "fix($(git_current_branch | cut -d / -f2)):'
 
-#Setup git config aliases
-alias swed="${currentDir}/swedconfig"
-alias my="${currentDir}/myconfig"
-chmod +x ${currentDir}/swedconfig
-chmod +x ${currentDir}/myconfig
-
-#Set default git config
-swed
+#TODO: Check again if this one works properly
+#git config --global includeIf.gitdir:**/swedbank/.path ~/.dotfiles/swedbank/.gitconfig
