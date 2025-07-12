1654e9b550f525ed96dbab3b9f3f74299870caee
pkey <paulius.kutka@snyk.io>
2025-07-12 02:53:38 +0000

    fix: add more tmux aliases

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,6 @@
+# Sources .zshrc
+alias reload='source ~/.zshrc'
+
 alias -- -='cd -'
 alias ..='cd ../'
 alias ...=../..
@@ -81,12 +84,37 @@ alias muxad='tmux attach -d'
 alias muxnew='new_tmux_and_switch'
 alias muxkillall='tmux kill-server'
 alias muxkillo='tmux kill-session -a'
-alias muxsw='tmux switch-client -t'
+
+muxsw() {
+    if [ -z "$1" ]; then
+        # Use tmux's built-in last session switching
+        tmux switch-client -l
+    else
+        tmux switch-client -t "$1"
+    fi
+}
+
 # Renames current session
 alias muxrn='tmux rename-session '
 alias muxls='tmux list-sessions'
 alias muxns='tmux new-session'
 alias muxks='tmux kill-session'
+
+# Window management
+alias muxd='tmux split-window -h'  # Split vertically (left/right)
+alias muxh='tmux split-window -v'  # Split horizontally (top/bottom)
+alias muxw='tmux kill-pane'        # Close current pane
+alias muxn='tmux new-window'       # New window
+alias muxlw='tmux list-windows'     # List windows
+
+## Window switching
+alias muxwn='tmux next-window'     # Next window
+alias muxwp='tmux previous-window' # Previous window
+alias muxw1='tmux select-window -t :1'  # Switch to window 1
+alias muxw2='tmux select-window -t :2'  # Switch to window 2
+alias muxw3='tmux select-window -t :3'  # Switch to window 3
+alias muxw4='tmux select-window -t :4'  # Switch to window 4
+alias muxw5='tmux select-window -t :5'  # Switch to window 5
 
 #Workflow
 alias test-update='jest --only-changed -u'
