d8e74cd95fc07a9cb6239674780acd8d9e8545f4
pkey <13101751+pkey@users.noreply.github.com>
2026-01-19 08:37:50 +0000

    fix: add help work aliases

--- aliases.sh
+++ aliases.sh
@@ -1,83 +1,76 @@
-# OGG
-# Sources .zshrc and ensures symlinks are up to date
-alias reload='$DOTFILES/setup_symlinks.sh && source ~/.zshrc'
+# Shell
+alias reload='$DOTFILES/setup_symlinks.sh && source ~/.zshrc'                # Reload shell config
 
-# Cursor
-alias cur='cursor .'
+# Editor
+alias cur='cursor .'                                                         # Open current dir in Cursor
+alias ealias='nvim $DOTFILES/aliases.sh'                                     # Edit aliases file
+alias edot='nvim $DOTFILES'                                                  # Edit dotfiles dir
+alias vim=nvim                                                               # Use neovim as vim
 
-# Quick Edits
-alias ealias='nvim $DOTFILES/aliases.sh'
-alias edot='nvim $DOTFILES'
-
-# Quick CDs
-alias cdot='cd $DOTFILES'
-
-#nvim
-alias vim=nvim
-
-#cd
-alias -- -='cd -'
-alias ..='cd ../'
-alias ...=../..
-alias ....=../../..
-alias .....=../../../..
-alias ......=../../../../..
-alias 1='cd -'
-alias 2='cd -2'
-alias 3='cd -3'
-alias 4='cd -4'
-alias 5='cd -5'
-alias 6='cd -6'
-alias 7='cd -7'
-alias 8='cd -8'
-alias 9='cd -9'
-alias _=sudo
-alias checkout='git checkout $(git branch | fzf)'
-alias l='ls -lah'
-alias la='ls -lAh'
-alias ll='ls -lh'
-alias ls='ls -G'
-alias lsa='ls -lah'
-alias md='mkdir -p'
-alias run-help=man
-alias which-command=whence
+# Navigation
+alias -- -='cd -'                                                            # Go to previous dir
+alias ..='cd ../'                                                            # Up one level
+alias ...=../..                                                              # Up two levels
+alias ....=../../..                                                          # Up three levels
+alias .....=../../../..                                                      # Up four levels
+alias ......=../../../../..                                                  # Up five levels
+alias 1='cd -'                                                               # Go to previous dir
+alias 2='cd -2'                                                              # Go back 2 dirs
+alias 3='cd -3'                                                              # Go back 3 dirs
+alias 4='cd -4'                                                              # Go back 4 dirs
+alias 5='cd -5'                                                              # Go back 5 dirs
+alias 6='cd -6'                                                              # Go back 6 dirs
+alias 7='cd -7'                                                              # Go back 7 dirs
+alias 8='cd -8'                                                              # Go back 8 dirs
+alias 9='cd -9'                                                              # Go back 9 dirs
+alias _=sudo                                                                 # Shortcut for sudo
+alias checkout='git checkout $(git branch | fzf)'                            # Fuzzy checkout branch
+alias l='ls -lah'                                                            # List all with details
+alias la='ls -lAh'                                                           # List almost all with details
+alias ll='ls -lh'                                                            # List with details
+alias ls='ls -G'                                                             # List with colors
+alias lsa='ls -lah'                                                          # List all with details
+alias md='mkdir -p'                                                          # Make dir with parents
+alias run-help=man                                                           # Show manual page
+alias which-command=whence                                                   # Show command location
+alias cdot='cd $DOTFILES'                                                    # Go to dotfiles dir
 
 # Git
+alias g=git                                                                  # Git shortcut
+alias ga='git add'                                                           # Stage files
+alias gaa='git add --all'                                                    # Stage all changes
+alias gap='git apply'                                                        # Apply patch
+alias gb='git branch'                                                        # List branches
+alias 'gc!'='git commit -v --amend'                                          # Amend last commit
+alias gc='git commit'                                                        # Commit staged changes
+alias gai='aicommit'                                                         # AI-generated commit msg
+alias gc-='git checkout -'                                                   # Checkout previous branch
+alias 'gca!'='git commit -v -a --amend'                                      # Amend with all changes
+alias grw='gaa && gc! --no-edit && gp!'                                      # Rewrite: stage, amend, push
+alias gcb='git checkout -b'                                                  # Create and checkout branch
+alias gci='git checkout project/integration'                                 # Checkout integration
+alias gco='git checkout'                                                     # Checkout branch/file
+alias ggpush='git push origin $(git_current_branch)'                         # Push to origin
+alias 'gp!'='ggpush --force-with-lease'                                      # Force push safely
+alias ggsup='git branch --set-upstream-to=origin/$(git_current_branch)'      # Set upstream
+alias gl='git pull'                                                          # Pull from remote
+alias gp='git push'                                                          # Push to remote
+alias gpsup='git push --set-upstream origin $(git_current_branch)'           # Push and set upstream
+alias grb='git rebase'                                                       # Rebase branch
+alias grba='git rebase --abort'                                              # Abort rebase
+alias grbc='git rebase --continue'                                           # Continue rebase
+alias grbs='git rebase --skip'                                               # Skip rebase commit
+alias grbi='git rebase -i'                                                   # Interactive rebase
+alias gst='git status'                                                       # Show working tree status
+alias glr='git pull --rebase'                                                # Pull with rebase
+alias glrm='git pull --rebase origin master'                                 # Pull rebase from master
+alias grs='git reset --staged'                                               # Unstage files
+alias gro='git reset --hard origin/$(git_current_branch)'                    # Reset to origin
+alias gf='git fetch origin'                                                  # Fetch from origin
+alias grfo='gf && grb origin/$(git symbolic-ref refs/remotes/origin/HEAD | sed "s@refs/remotes/origin/@@")'  # Fetch and rebase default
 
-alias g=git
-alias ga='git add'
-alias gaa='git add --all'
-alias gap='git apply'
-alias gb='git branch'
-alias 'gc!'='git commit -v --amend'
-alias gc='git commit'
-alias gai='aicommit'
-alias gc-='git checkout -'
-alias 'gca!'='git commit -v -a --amend'
-alias grw='gaa && gc! --no-edit && gp!'
-alias gcb='git checkout -b'
-alias gci='git checkout project/integration'
-alias gco='git checkout'
-alias ggpush='git push origin $(git_current_branch)'
-alias 'gp!'='ggpush --force-with-lease'
-alias ggsup='git branch --set-upstream-to=origin/$(git_current_branch)'
-alias gl='git pull'
-alias gp='git push'
-alias gpsup='git push --set-upstream origin $(git_current_branch)'
-alias grb='git rebase'
-alias grba='git rebase --abort'
-alias grbc='git rebase --continue'
-alias grbi='git rebase -i'
-alias grbs='git rebase --skip'
-alias gst='git status'
-alias glr='git pull --rebase'
-alias glrm='git pull --rebase origin master'
-alias grs='git reset --staged'
-alias gro='git reset --hard origin/$(git_current_branch)'
-alias grfo='git fetch origin && git rebase origin/$(git symbolic-ref refs/remotes/origin/HEAD | sed "s@refs/remotes/origin/@@")'
-
-# Git Worktrees
-gwta() {
+# Git Worktree
+gwta() {                                                                     # Create worktree with new branch
   local branch="$1"
   local base="${2:-HEAD}"
   local dir_name="${branch//\//-}"
@@ -85,28 +78,28 @@ gwta() {
   git worktree add -b "$branch" "$worktree_path" "$base" && cd "$worktree_path"
 }
 
-gwtc() {
+gwtc() {                                                                     # Checkout branch in worktree
   local branch="$1"
   local dir_name="${branch//\//-}"
   local worktree_path="/tmp/$(basename $(git rev-parse --show-toplevel))-$dir_name"
   git worktree add "$worktree_path" "$branch" && cd "$worktree_path"
 }
 
-gwtl() {
+gwtl() {                                                                     # Fuzzy select worktree
   local selected
   selected=$(git worktree list | fzf --header="Select worktree" | awk '{print $1}')
   [[ -n "$selected" ]] && cd "$selected"
 }
 
-gwtd() {
+gwtd() {                                                                     # Fuzzy remove worktree
   local selected
   selected=$(git worktree list | fzf --header="Select worktree to remove" --multi | awk '{print $1}')
   [[ -n "$selected" ]] && echo "$selected" | xargs -I{} git worktree remove "{}" && cd ..
 }
 
-alias gwtls='git worktree list'
+alias gwtls='git worktree list'                                              # List all worktrees
 
-gwtla() {
+gwtla() {                                                                    # Fuzzy worktree all repos
   local search_dir="${1:-$HOME/repos}"
   local selected
   selected=$(fd -H -t d '^\.git$' "$search_dir" -x git -C {//} worktree list 2>/dev/null | \
@@ -116,98 +109,135 @@ gwtla() {
   [[ -n "$selected" ]] && cd "$selected"
 }
 
-#Vim
-alias v='vim'
+# Yarn
+alias ybw='yarn build --watch'                                               # Build with watch mode
+alias yl='yarn lint --fix'                                                   # Lint and fix
+alias yt='yarn test'                                                         # Run tests
 
-#Yarn
-alias ybw='yarn build --watch'
-alias yl='yarn lint --fix'
-alias yt='yarn test'
+# Kubectl
+alias k='kubectl'                                                            # Kubectl shortcut
 
-
-#Kubectl
-alias k='kubectl'
-
-
-new_tmux_and_switch() {
+# Tmux
+new_tmux_and_switch() {                                                      # Create and switch session
   local name="$1"
   tmux switch-client -t "$(tmux new-session -d -P -s "$name")"
 }
 
-#Tmux
-alias mux='tmux'
-alias muxa='tmux attach'
-alias muxad='tmux attach -d'
-alias muxnew='new_tmux_and_switch'
-alias muxkillall='tmux kill-server'
-alias muxkillo='tmux kill-session -a'
+alias mux='tmux'                                                             # Tmux shortcut
+alias muxa='tmux attach'                                                     # Attach to session
+alias muxad='tmux attach -d'                                                 # Attach detach others
+alias muxnew='new_tmux_and_switch'                                           # New session and switch
+alias muxkillall='tmux kill-server'                                          # Kill tmux server
+alias muxkillo='tmux kill-session -a'                                        # Kill other sessions
 
-muxsw() {
+muxsw() {                                                                    # Switch session
     if [ -z "$1" ]; then
-        # Use tmux's built-in last session switching
         tmux switch-client -l
     else
         tmux switch-client -t "$1"
     fi
 }
 
-# Renames current session
-alias muxrn='tmux rename-session '
-alias muxls='tmux list-sessions'
-alias muxns='tmux new-session'
-alias muxks='tmux kill-session'
+alias muxrn='tmux rename-session '                                           # Rename current session
+alias muxls='tmux list-sessions'                                             # List sessions
+alias muxns='tmux new-session'                                               # New session
+alias muxks='tmux kill-session'                                              # Kill session
+alias muxd='tmux split-window -h'                                            # Split pane vertical
+alias muxh='tmux split-window -v'                                            # Split pane horizontal
+alias muxw='tmux kill-pane'                                                  # Close current pane
+alias muxn='tmux new-window'                                                 # New window
+alias muxlw='tmux list-windows'                                              # List windows
+alias muxwn='tmux next-window'                                               # Next window
+alias muxwp='tmux previous-window'                                           # Previous window
+alias muxw1='tmux select-window -t :1'                                       # Go to window 1
+alias muxw2='tmux select-window -t :2'                                       # Go to window 2
+alias muxw3='tmux select-window -t :3'                                       # Go to window 3
+alias muxw4='tmux select-window -t :4'                                       # Go to window 4
+alias muxw5='tmux select-window -t :5'                                       # Go to window 5
 
-# Window management
-alias muxd='tmux split-window -h'  # Split vertically (left/right)
-alias muxh='tmux split-window -v'  # Split horizontally (top/bottom)
-alias muxw='tmux kill-pane'        # Close current pane
-alias muxn='tmux new-window'       # New window
-alias muxlw='tmux list-windows'     # List windows
+# Workflow
+alias test-update='jest --only-changed -u'                                   # Update test snapshots
+alias trypush='trypush_fn'                                                   # Test format amend push
+alias jest='npx jest'                                                        # Run jest via npx
+alias lookIWorked='$(findUnpushedCommits)'                                   # Find unpushed commits
+alias dateUpdate='GIT_COMMITTER_DATE="$(date)" git commit --amend --no-edit --date "$(date)"'  # Update commit date
 
-## Window switching
-alias muxwn='tmux next-window'     # Next window
-alias muxwp='tmux previous-window' # Previous window
-alias muxw1='tmux select-window -t :1'  # Switch to window 1
-alias muxw2='tmux select-window -t :2'  # Switch to window 2
-alias muxw3='tmux select-window -t :3'  # Switch to window 3
-alias muxw4='tmux select-window -t :4'  # Switch to window 4
-alias muxw5='tmux select-window -t :5'  # Switch to window 5
-
-#Workflow
-alias test-update='jest --only-changed -u'
-alias trypush='(! git diff HEAD develop --exit-code --quiet) && (! git diff HEAD master --exit-code --quiet) && git status && (jest --changedSince develop || true) && (npm run format || true) && git commit --amend -a --no-edit && git push -u --force-with-lease'
-alias jest='npx jest'
-
-### Find unpushed commits ###
-
-alias lookIWorked='$(findUnpushedCommits)'
-alias dateUpdate='GIT_COMMITTER_DATE="$(date)" git commit --amend --no-edit --date "$(date)"'
+trypush_fn() {                                                               # Test format amend push
+  (! git diff HEAD develop --exit-code --quiet) && \
+  (! git diff HEAD master --exit-code --quiet) && \
+  git status && \
+  (jest --changedSince develop || true) && \
+  (npm run format || true) && \
+  git commit --amend -a --no-edit && \
+  git push -u --force-with-lease
+}
 
 # Bazel
-_bazel_fuzzy_run() {
+_bazel_fuzzy_run() {                                                         # Fuzzy run bazel target
   local target
   target=$(bazel query 'kind(".*_binary|.*_test", //...)' | fzf)
   [[ -n "$target" ]] && bazel run "$target"
 }
-alias bzlrun="_bazel_fuzzy_run"
+alias bzlrun="_bazel_fuzzy_run"                                              # Fuzzy run bazel target
 
-_bazel_fuzzy_test() {
+_bazel_fuzzy_test() {                                                        # Fuzzy test bazel target
   local target
   target=$(bazel query 'kind(".*_test", //...)' | fzf)
   [[ -n "$target" ]] && bazel test "$target"
 }
-alias bzltest="_bazel_fuzzy_test"
+alias bzltest="_bazel_fuzzy_test"                                            # Fuzzy test bazel target
 
-# Vast.ai
-alias vast="vastai"
+# Vast
+alias vast="vastai"                                                          # Vast.ai CLI shortcut
 
-# Focus workflow
-alias focus='sudo $HOME/dotfiles/productivity/focus block'
-alias unfocus='sudo $HOME/dotfiles/productivity/focus unblock'
+# Focus
+alias focus='sudo $HOME/dotfiles/productivity/focus block'                   # Block distracting sites
+alias unfocus='sudo $HOME/dotfiles/productivity/focus unblock'               # Unblock distracting sites
 
-# docker compose in many places is referred as docker-compose
-alias docker-compose="docker compose"
+# Docker
+alias docker-compose="docker compose"                                        # Use new docker compose
 
 # Claude
-alias cc="claude"
-alias eclaude='vim $DOTFILES/claude/CLAUDE.md'
+alias cc="claude"                                                            # Claude CLI shortcut
+alias eclaude='vim $DOTFILES/claude/CLAUDE.md'                               # Edit Claude config
+
+# Help
+alias-help() {                                                               # Show aliases with desc
+  local filter="${1:-}"
+  local current_section=""
+  local printed_section=""
+
+  while IFS= read -r line; do
+    if [[ "$line" == \#* ]] && [[ "$line" != \#\!* ]]; then
+      current_section="${line#\#}"
+      current_section="${current_section# }"
+    elif [[ "$line" == alias\ * ]]; then
+      local rest="${line#alias }"
+      local name="${rest%%=*}"
+      local cmd_and_comment="${rest#*=}"
+      local cmd="${cmd_and_comment%%  #*}"
+      local comment=""
+      [[ "$cmd_and_comment" == *"  #"* ]] && comment="${cmd_and_comment##*  # }"
+      name="${name//\'/}"
+      name="${name//\"/}"
+      cmd="${cmd#\'}"
+      cmd="${cmd#\"}"
+      cmd="${cmd%\'}"
+      cmd="${cmd%\"}"
+
+      if [[ -z "$filter" ]] || [[ "$name" == *"$filter"* ]] || [[ "${current_section:l}" == *"${filter:l}"* ]]; then
+        if [[ "$current_section" != "$printed_section" ]]; then
+          printf "\n\033[1;34m── %s ──\033[0m\n" "$current_section"
+          printed_section="$current_section"
+        fi
+        if [[ -n "$comment" ]]; then
+          printf "  \033[1;32m%-15s\033[0m \033[0;33m%-26s\033[0m %s\n" "$name" "$comment" "$cmd"
+        else
+          printf "  \033[1;32m%-15s\033[0m %s\n" "$name" "$cmd"
+        fi
+      fi
+    fi
+  done < "$DOTFILES/aliases.sh"
+}
+
+alias halias='alias-help'                                                    # Alias help shortcut
