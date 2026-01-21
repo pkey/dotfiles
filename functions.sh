960b124bba273d5b6390a73c9fe5bc734cc8f9f3
pkey <13101751+pkey@users.noreply.github.com>
2026-01-21 16:24:33 +0000

    fix: auto switch go functions

--- functions.sh
+++ functions.sh
@@ -36,6 +36,42 @@ findUnpushedCommits () {
   find ./ -type d -maxdepth 3 -exec sh -c 'cd "$1" && git cherry -v' _ {} \;
 }
 
+#--Go
+_go_version() {
+  [[ -f "go.mod" ]] && grep '^go ' go.mod | awk '{print $2}'
+}
+
+go() {
+  local version=$(_go_version)
+  if [[ -n "$version" ]]; then
+    local wrapper="$HOME/go/bin/go${version}"
+    if [[ ! -x "$wrapper" ]]; then
+      echo "Installing Go $version..."
+      command go install "golang.org/dl/go${version}@latest"
+      "$wrapper" download
+    fi
+    if [[ -x "$wrapper" ]]; then
+      "$wrapper" "$@"
+      return
+    fi
+  fi
+  command go "$@"
+}
+
+_go_chpwd() {
+  local version=$(_go_version)
+  if [[ -n "$version" ]]; then
+    local wrapper="$HOME/go/bin/go${version}"
+    if [[ -x "$wrapper" ]]; then
+      echo "Using Go $version (from go.mod)"
+    else
+      echo "Go $version required (run 'go version' to install)"
+    fi
+  fi
+}
+autoload -Uz add-zsh-hook
+add-zsh-hook chpwd _go_chpwd
+
 #TODO: allow dynamic prompt (based on environment)
 #TODO: allow using models beyond local ollama
 aicommit () {
