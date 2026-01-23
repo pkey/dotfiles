e49d149b96c4a54949dc9b8a6a355ff12e7b00c9
pkey <13101751+pkey@users.noreply.github.com>
2026-01-23 17:07:43 +0000

    fix: supress input

--- scaffolding/ts/init.sh
+++ scaffolding/ts/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-npm init -y
+npm init -y > /dev/null
 npm pkg set scripts.start="tsx src/index.ts"
 npm pkg set scripts.test="vitest"
 npm install --save-dev tsx @types/node vitest
